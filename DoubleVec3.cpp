2becb7d1cb2206885ae8582d0afc8924b783d47f
Joachim Favre <joachim@favre.xyz>
2020-07-02 12:29:13 +0200

    Defined constructors for DoubleVec3

--- DoubleVec3.cpp
+++ DoubleVec3.cpp
@@ -1,2 +1,16 @@
 #include "DoubleVec3.h"
 
+DoubleVec3::DoubleVec3(double val /*= 0*/) : x(val), y(val), z(val) {}
+
+DoubleVec3::DoubleVec3(double x, double y, double z) {
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+DoubleVec3::DoubleVec3(const DoubleVec3& vec) {
+	this->x = vec.getX();
+	this->y = vec.getY();
+	this->z = vec.getZ();
+}
+
