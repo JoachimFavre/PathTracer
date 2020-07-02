183df78683822c21f999be0916b15a1130c9e766
Joachim Favre <joachim@favre.xyz>
2020-07-02 15:01:26 +0200

    Defined header for class Object3D

--- Sphere.h
+++ Sphere.h
@@ -2,10 +2,26 @@
 #define DEF_SPHERE
 
 #include "Object3D.h";
+#include "DoubleVec3.h"
+#include "Ray.h"
 
 class Sphere : public Object3D {
+private:
+	DoubleVec3 center;
+	double radius;
 
+public:
+	Sphere();
+	Sphere(const DoubleVec3& center, double radius, const DoubleVec3& colour, BRDF brdf, double emittance = 0);
+	Sphere(const Sphere& sphere);
 
+	DoubleVec3 getCenter() const;
+	double getRadius() const;
+
+	void setCenter(const DoubleVec3& center);
+	void setRadius(double radius);
+
+	double closestIntersection(const Ray& ray) const;
 };
 
 #endif
