d360fdb1c63bf7a3197772ae45a39be84c173962
Joachim Favre <joachim@favre.xyz>
2020-07-02 14:46:05 +0200

    Defined getters and setters for Object3D class

--- Object3D.cpp
+++ Object3D.cpp
@@ -8,3 +8,14 @@ Object3D::Object3D(const DoubleVec3& colour, BRDF brdf, double emittance /* = 0*
 
 Object3D::Object3D(const Object3D& obj) 
 	: colour(obj.colour), brdf(obj.brdf), emittance(obj.emittance) {}
+
+
+// Getters
+DoubleVec3 Object3D::getColour() const { return colour; }
+BRDF Object3D::getBRDF() const { return brdf; }
+double Object3D::getEmittance() const { return emittance; }
+
+// Setters
+void Object3D::setColour(const DoubleVec3& colour) { this->colour = colour; }
+void Object3D::setBRDF(BRDF brdf) { this->brdf = brdf; }
+void Object3D::setEmittance(double emittance) { this->emittance = emittance; }
