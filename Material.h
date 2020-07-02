56d2f3aff6a3efcabd55e91add3c9b490c8e52c1
Joachim Favre <joachim@favre.xyz>
2020-07-02 15:39:56 +0200

    Defined the header for the Material class

--- Material.h
+++ Material.h
@@ -1,8 +1,27 @@
 #ifndef DEF_SPHERE
 #define DEF_SPHERE
 
-class Material {
+#include "DoubleVec3.h"
+#include "BRDF.h"
 
+class Material {
+private:
+	DoubleVec3 colour;
+	BRDF brdf;
+	double emittance;
+
+private:
+	Material();
+	Material(const DoubleVec3& colour, BRDF brdf, double emittance = 0);
+	Material(const Material& obj);
+
+	DoubleVec3 getColour() const;
+	BRDF getBRDF() const;
+	double getEmittance() const;
+
+	void setColour(const DoubleVec3& colour);
+	void setBRDF(BRDF brdf);
+	void setEmittance(double emittance);
 };
 
 
