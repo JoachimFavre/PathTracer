52488f786740018db5f992589bcefddfce7cf5c3
Joachim Favre <joachim@favre.xyz>
2020-07-02 14:38:01 +0200

    Defined header and constructors for Object3D class

--- Object3D.cpp
+++ Object3D.cpp
@@ -1,2 +1,10 @@
 #include "Object3D.h"
 
+// Constructors
+Object3D::Object3D() : colour(0), brdf(BRDF::Diffuse), emittance(0) {}
+
+Object3D::Object3D(const DoubleVec3& colour, BRDF brdf, double emittance /* = 0*/)
+	: colour(colour), brdf(brdf), emittance(emittance) {}
+
+Object3D::Object3D(const Object3D& obj) 
+	: colour(obj.colour), brdf(obj.brdf), emittance(obj.emittance) {}
--- Object3D.h
+++ Object3D.h
@@ -1,10 +1,30 @@
 #ifndef DEF_OBJECT3D
 #define DEF_OBJECT3D
 
+#include "DoubleVec3.h"
+#include "Ray.h"
+#include "BRDF.h"
+
 class Object3D {
+private:
+	DoubleVec3 colour;
+	BRDF brdf;
+	double emittance;
 
+public:
+	Object3D();
+	Object3D(const DoubleVec3& colour, BRDF brdf, double emittance = 0);
+	Object3D(const Object3D& obj);
 
+	DoubleVec3 getColour() const;
+	BRDF getBRDF() const;
+	double getEmittance() const;
+
+	void setColour(const DoubleVec3& colour);
+	void setBRDF(BRDF brdf);
+	void setEmittance(double emittance);
+
+	virtual double closestIntersection(const Ray& ray) const = 0;
 };
-
 
 #endif
