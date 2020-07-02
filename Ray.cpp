94580a6318f1e25c667776d6cac557a8d6cf98af
Joachim Favre <joachim@favre.xyz>
2020-07-02 14:11:39 +0200

    Added constructors, getters and setters for the Ray object

--- Ray.cpp
+++ Ray.cpp
@@ -1,1 +1,35 @@
 #include "Ray.h"
+
+// Constructors
+Ray::Ray() : origin(0), direction(1, 0, 0) {}
+
+Ray::Ray(const DoubleVec3& origin, const DoubleVec3& direction)
+	: origin(origin), direction(direction) {
+	this->direction.normalise();
+}
+
+Ray::Ray(const Ray& ray) {
+	this->origin = ray.getOrigin();
+	this->direction = ray.getDirection();
+}
+
+
+// Getters
+DoubleVec3 Ray::getOrigin() const {
+	return origin;
+}
+
+DoubleVec3 Ray::getDirection() const {
+	return direction;
+}
+
+
+// Setters
+void Ray::setOrigin(const DoubleVec3& origin) {
+	this->origin = origin;
+}
+
+void Ray::setDirection(const DoubleVec3& direction) {
+	this->direction = direction;
+}
+
--- Ray.h
+++ Ray.h
@@ -1,8 +1,22 @@
 #ifndef DEF_RAY
 #define DEF_RAY
 
-class Ray {
+#include "DoubleVec3.h"
 
+class Ray {
+private:
+	DoubleVec3 origin, direction;
+
+public:
+	Ray();
+	Ray(const DoubleVec3& origin, const DoubleVec3& direction);
+	Ray(const Ray& ray);
+
+	DoubleVec3 getOrigin() const;
+	DoubleVec3 getDirection() const;
+
+	void setOrigin(const DoubleVec3& origin);
+	void setDirection(const DoubleVec3& direction);
 };
 
 #endif
