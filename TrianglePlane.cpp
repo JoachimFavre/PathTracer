93d3d39f0c3376f2cc0ed4d537ae3583288eac3c
Joachim Favre <joachim@favre.xyz>
2020-07-03 11:33:56 +0200

    Added closestIntersection and getNormat methods to TrianglePlane class

--- TrianglePlane.cpp
+++ TrianglePlane.cpp
@@ -17,9 +17,36 @@ DoubleVec3 TrianglePlane::getVertex2() const { return vertex2; }
 DoubleVec3 TrianglePlane::getVertex3() const { return vertex3; }
 
 
-//Setters
+// Setters
 void TrianglePlane::setVertex1(const DoubleVec3& vertex) { vertex1 = vertex; }
 void TrianglePlane::setVertex2(const DoubleVec3& vertex) { vertex2 = vertex; }
 void TrianglePlane::setVertex3(const DoubleVec3& vertex) { vertex3 = vertex; }
 
 
+// Other methods
+double TrianglePlane::closestIntersection(const Ray& ray) const {
+	// Using Möller-Trumbore intersection algorithm (using notations from https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm (accessed on 3rd July 2020)
+	// Return -1 if no intersection
+	DoubleVec3 edge1 = vertex2 - vertex1;
+	DoubleVec3 edge2 = vertex3 - vertex1;
+	DoubleVec3 h = crossProd(ray.getDirection(), edge2);
+	double a = dotProd(edge1, h);
+	if (a > -DBL_EPSILON && a < DBL_EPSILON)
+		return -1;  // Triangle and ray are parallel
+	double f = 1.0 / a;
+	DoubleVec3 s = ray.getOrigin() - vertex1;
+	double barycentricCoordU = f * dotProd(s, h);
+	if (barycentricCoordU < 0.0 || barycentricCoordU > 1.0)  // conditions for barycentric coordinates
+		return -1;
+	DoubleVec3 q = crossProd(s, edge1);
+	double barycentricCoordV = f * dotProd(ray.getDirection(), q);
+	if (barycentricCoordV < 0.0 || barycentricCoordU + barycentricCoordV > 1.0)  // conditions for barycentric coordinates
+		return -1;
+	return f * dotProd(edge2, q);
+}
+
+DoubleVec3 TrianglePlane::getNormal(const DoubleVec3& point) const {
+	DoubleVec3 edge1 = vertex2 - vertex1;
+	DoubleVec3 edge2 = vertex3 - vertex1;
+	return crossProd(edge1, edge2);  // Not sure with the signs
+}
