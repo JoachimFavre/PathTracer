0b2f63c0534687a08a0c9a6421394d1019be7de0
Joachim Favre <joachim@favre.xyz>
2020-07-03 15:10:14 +0200

    Diffuse BRDF and recursivity added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <random>
+#include <chrono>
 
 #define _USE_MATH_DEFINES  // to be able to use M_PI from math.h
 #include <math.h>
@@ -14,6 +16,9 @@
 #include "DoubleVec3.h"
 #include "Material.h"
 
+std::uniform_real_distribution<double> unif(0, 1);
+std::default_random_engine re(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());  // new seed each time
+
 std::vector<Object3D*> scene;
 
 constexpr unsigned int PICTURE_WIDTH = 500;
@@ -23,32 +28,65 @@ constexpr double CAMERA_FOV_X = M_PI_4;
 
 constexpr double MAX_DEPTH = 10;
 constexpr unsigned int MAX_BOUNCES = 5;
-constexpr unsigned int SAMPLE_PER_PIXEL = 8;
+constexpr unsigned int SAMPLE_PER_PIXEL = 500;
 
-DoubleVec3 traceRay(const Ray& ray, DoubleVec3 colour = DoubleVec3(0), unsigned int bounces = 0) {
-	double smallestPositiveDistance = MAX_DEPTH + 1;  // Strictly positive -> we don't want it to intersect with same object
-	Object3D* closestObject = nullptr;
-	for (Object3D* object : scene) {
-		double distance = object->closestIntersection(ray);
-		if (distance > DBL_EPSILON && distance < smallestPositiveDistance) {
-			smallestPositiveDistance = distance;
-			closestObject = object;
+double randomDouble() {
+	return unif(re);
+}
+
+DoubleVec3 newDirectionOnHemisphere(const DoubleVec3& normal) {
+	// Generate vector on sphere and then check if angle with normal is between -pi/2 and pi/2
+	double cos_angle = -1;
+	DoubleVec3 randomVec;
+	while (cos_angle < 0) {
+		double theta = M_PI*randomDouble() - M_PI_2;
+		double phi = M_PI*randomDouble() - M_PI_2;
+		randomVec = DoubleVec3(sin(theta)*cos(phi), sin(theta)*sin(phi), cos(theta));
+		cos_angle = dotProd(randomVec, normal);
+	}
+	return randomVec;
+}
+
+DoubleVec3 traceRay(const Ray& ray, DoubleVec3 colour = DoubleVec3(0), unsigned int bounces = 0) {  // is "colour" necessary ?
+	if (bounces < MAX_BOUNCES) {
+		double smallestPositiveDistance = MAX_DEPTH + 1;  // Strictly positive -> we don't want it to intersect with same object
+		Object3D* closestObject = nullptr;
+		for (Object3D* object : scene) {
+			double distance = object->closestIntersection(ray);
+			if (distance > DBL_EPSILON&& distance < smallestPositiveDistance) {
+				smallestPositiveDistance = distance;
+				closestObject = object;
+			}
 		}
+		if (smallestPositiveDistance < MAX_DEPTH) {
+			Material objectMaterial = closestObject->getMaterial();
+			DoubleVec3 objectColour = objectMaterial.getColour();
+			colour += DoubleVec3(objectMaterial.getEmittance());
+			// assume BRDF is diffuse
+			DoubleVec3 intersection = ray.getOrigin() + smallestPositiveDistance * ray.getDirection();
+			DoubleVec3 normal = closestObject->getNormal(intersection);
+			DoubleVec3 newDirection = newDirectionOnHemisphere(normal);
+			Ray newRay = Ray(intersection, newDirection);
+			DoubleVec3 recursiveColour = traceRay(newRay, DoubleVec3(0), bounces + 1);
+			colour += DoubleVec3(recursiveColour.getX()*objectColour.getX(),
+					             recursiveColour.getY()*objectColour.getY(),
+				                 recursiveColour.getZ()*objectColour.getZ())
+				      * dotProd(ray.getDirection(), normal);
+			return colour;
+		}
+		else  // did not hit anything
+			return colour;
 	}
-	if (smallestPositiveDistance < MAX_DEPTH) {
-		Material ObjectMaterial = closestObject->getMaterial();
-		colour += DoubleVec3(ObjectMaterial.getEmittance());
-		// To implement : BRDF and recursion
-		return colour;
-	}
-	else  // did not hit anything
+	else // max bounces
 		return colour;
 }
 
 int main() {
-	scene.push_back(new Sphere(DoubleVec3(0, 0, -5), 1, Material(DoubleVec3(5), BRDF::Diffuse, 200)));
-	//scene.push_back(new TrianglePlane(DoubleVec3(1, 1, -5), DoubleVec3(1, 0, -5), DoubleVec3(0, 1, -5), Material(DoubleVec3(5), BRDF::Diffuse, 1000)));
-
+	// Make scene
+	scene.push_back(new Sphere(DoubleVec3(0, 0, -5), 1, Material(DoubleVec3(5), BRDF::Diffuse, 0)));
+	scene.push_back(new Sphere(DoubleVec3(-1, 1, -3), 0.2, Material(DoubleVec3(5), BRDF::Diffuse, 1000)));
+	//scene.push_back(new Triangle(DoubleVec3(0, )))
+	
 	PerspectiveCamera camera(PICTURE_WIDTH, PICTURE_HEIGHT, CAMERA_FOV_X);
 
 	// Trace
