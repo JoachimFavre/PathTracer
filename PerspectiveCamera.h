90d990203b394dd7601cbc49ea76bd1f3e4c02cc
Joachim Favre <joachim@favre.xyz>
2020-07-03 11:44:41 +0200

    Defined header for the PerspectiveCamera class

--- PerspectiveCamera.h
+++ PerspectiveCamera.h
@@ -1,8 +1,28 @@
 #ifndef DEF_PERSPECTIVECAMERA
 #define DEF_PERSPECTIVECAMERA
 
-class PerspectiveCamera {
+#include "DoubleVec3.h"
+#include "Ray.h"
 
+class PerspectiveCamera {
+private:
+	unsigned int width, height;
+	double fovX;
+
+public:
+	PerspectiveCamera();
+	PerspectiveCamera(unsigned int width, unsigned int height, double fovX);
+	PerspectiveCamera(const PerspectiveCamera& camera);
+	
+	unsigned int getWidth();
+	unsigned int getHeigt();
+	double getFovX();
+
+	void setWidth(unsigned int width);
+	void setHeight(unsigned int height);
+	void setFovX(double fovX);
+
+	Ray getRayGoingThrough(unsigned int pixelX, unsigned int pixelY);
 };
 
 #endif
