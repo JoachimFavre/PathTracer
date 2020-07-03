d94b2d9dab0008419ded846086f08d892d338d3c
Joachim Favre <joachim@favre.xyz>
2020-07-03 11:01:23 +0200

    Added getters and setters to the TrianglePlane class

--- TrianglePlane.cpp
+++ TrianglePlane.cpp
@@ -10,3 +10,16 @@ TrianglePlane::TrianglePlane(const DoubleVec3& vertex1, const DoubleVec3& vertex
 TrianglePlane::TrianglePlane(const TrianglePlane& triangle)
 	: Object3D(triangle), vertex1(triangle.vertex1), vertex2(triangle.vertex2), vertex3(triangle.vertex3) {}
 
+
+// Getters
+DoubleVec3 TrianglePlane::getVertex1() const { return vertex1; }
+DoubleVec3 TrianglePlane::getVertex2() const { return vertex2; }
+DoubleVec3 TrianglePlane::getVertex3() const { return vertex3; }
+
+
+//Setters
+void TrianglePlane::setVertex1(const DoubleVec3& vertex) { vertex1 = vertex; }
+void TrianglePlane::setVertex2(const DoubleVec3& vertex) { vertex2 = vertex; }
+void TrianglePlane::setVertex3(const DoubleVec3& vertex) { vertex3 = vertex; }
+
+
