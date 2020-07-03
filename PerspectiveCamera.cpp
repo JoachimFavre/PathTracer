f4833673ca7259317640eb33f8bc16e3d371b81b
Joachim Favre <joachim@favre.xyz>
2020-07-03 11:53:14 +0200

    Defined getters and setters for PerspectiveCamera class

--- PerspectiveCamera.cpp
+++ PerspectiveCamera.cpp
@@ -11,3 +11,14 @@ PerspectiveCamera::PerspectiveCamera(const PerspectiveCamera& camera)
     : width(camera.width), height(camera.height), fovX(camera.fovX) {}
 
 
+// Getters
+unsigned int PerspectiveCamera::getWidth() { return width; }
+unsigned int PerspectiveCamera::getHeigt() { return height; }
+double PerspectiveCamera::getFovX() { return fovX; }
+
+
+// Setters
+void PerspectiveCamera::setWidth(unsigned int width) { this->width = width; }
+void PerspectiveCamera::setHeight(unsigned int height) { this->height = height; }
+void PerspectiveCamera::setFovX(double fovX) { this->fovX = fovX; }
+
