108760d6f5d79329a47032a144c590bb15bb62cd
Joachim Favre <joachim@favre.xyz>
2020-07-20 11:31:33 +0200

    Defined Picture class

---
+++ Picture.cpp
@@ -0,0 +1,47 @@
+#include "Picture.h"
+
+// Constructors & Destructors
+Picture::Picture() : Picture(500, 500) {}
+
+Picture::Picture(unsigned int width, unsigned int height) : width(width), height(height) {
+	pixels = new DoubleVec3D*[width];
+	for (int pixelX = 0; pixelX < width; pixelX++) {
+		pixels[pixelX] = new DoubleVec3D[height];
+		for (int pixelY = 0; pixelY < height; pixelY++)
+			pixels[pixelX][pixelY] = 0.0;
+	}
+}
+
+Picture::Picture(const Picture& picture) : Picture(picture.width, picture.height) {}
+
+Picture::~Picture() {
+	for (int pixelX = 0; pixelX < width; pixelX++)
+		delete pixels[pixelX];
+	delete pixels;
+}
+
+
+// Getters
+unsigned int Picture::getWidth() { return width; }
+unsigned int Picture::getHeight() { return height; }
+
+// Modify pixels
+void Picture::addValuePix(unsigned int x, unsigned int y, DoubleVec3D value) { pixels[x][y] += value; }
+void Picture::setValuePix(unsigned int x, unsigned int y, DoubleVec3D value) { pixels[x][y] = value; }
+
+// Other methods
+void Picture::write(double middleGray, std::string fileName /* = "picture"*/) {
+	std::ofstream file;
+	file.open(fileName + ".ppm");
+	file << "P3\n" << width << " " << height << "\n" << "255\n";
+	for (unsigned int pixelY = 0; pixelY < height; pixelY++) {
+		for (unsigned int pixelX = 0; pixelX < width; pixelX++) {
+			DoubleVec3D currentColour = pixels[pixelX][pixelY];
+			// Very naive tone mapping
+			file << (int)(255 * currentColour.getX() / (middleGray + currentColour.getX())) << " ";
+			file << (int)(255 * currentColour.getY() / (middleGray + currentColour.getY())) << " ";
+			file << (int)(255 * currentColour.getZ() / (middleGray + currentColour.getZ())) << "\n";
+		}
+	}
+	file.close();
+}
--- Picture.h
+++ Picture.h
@@ -1,9 +1,32 @@
+#include <fstream>
+#include <iostream>;
+#include <string>;
+
+#include "DoubleVec3D.h"
+
 #ifndef DEF_PICTURE
 #define DEF_PICTURE
 
 class Picture
 {
+private:
+	const unsigned int width;
+	const unsigned int height;
+	DoubleVec3D** pixels;
 
+public:
+	Picture();
+	Picture(unsigned int width, unsigned int height);
+	Picture(const Picture& picture);
+	~Picture();
+
+	unsigned int getWidth();
+	unsigned int getHeight();
+
+	void addValuePix(unsigned int x, unsigned int y, DoubleVec3D value);
+	void setValuePix(unsigned int x, unsigned int y, DoubleVec3D value);
+
+	void write(double middleGray, std::string fileName = "picture");
 };
 
 #endif
