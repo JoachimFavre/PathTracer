101bba74fc520b7c9bbd52566f6d1fb06ada4958
Joachim Favre <joachim@favre.xyz>
2020-07-23 15:04:39 +0200

    Added constructors, overloaded setVals method and operator=

--- DoubleUnitVec3D.cpp
+++ DoubleUnitVec3D.cpp
@@ -1,1 +1,32 @@
 #include "DoubleUnitVec3D.h"
+
+// Constructors
+DoubleUnitVec3D::DoubleUnitVec3D()
+	: DoubleVec3D(1, 0, 0) {}  // alerady normalised
+
+DoubleUnitVec3D::DoubleUnitVec3D(double x, double y, double z) 
+	: DoubleVec3D(x, y, z) {
+	normalise();
+}
+
+DoubleUnitVec3D::DoubleUnitVec3D(const DoubleVec3D& vec)
+	: DoubleVec3D(vec) {
+	normalise();
+}
+
+DoubleUnitVec3D::DoubleUnitVec3D(const DoubleUnitVec3D& vec) 
+	: DoubleVec3D(vec) {}  // already normalised
+
+
+// Setter
+void DoubleUnitVec3D::setVals(double x, double y, double z) {
+	DoubleVec3D::setVals(x, y, z);
+	normalise();
+}
+
+
+// Assignment operator
+void DoubleUnitVec3D::operator=(const DoubleVec3D& vec) {
+	DoubleVec3D::operator=(vec);
+	normalise();
+}
--- DoubleUnitVec3D.h
+++ DoubleUnitVec3D.h
@@ -5,8 +5,15 @@
 
 class DoubleUnitVec3D : public DoubleVec3D
 {
+public:
+	DoubleUnitVec3D();
+	DoubleUnitVec3D(double x, double y, double z);
+	DoubleUnitVec3D(const DoubleVec3D& vec);
+	DoubleUnitVec3D(const DoubleUnitVec3D& vec);
 
+	virtual void setVals(double x, double y, double z);
 
+	void operator=(const DoubleVec3D& vec);
 };
 
 #endif
