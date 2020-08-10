ff097ec76130cac955dde62ce95c8e660b5498ab
Joachim Favre <joachim@favre.xyz>
2020-08-10 12:11:14 +0200

    Added constructors, getters and setters to the DoubleMatrix33 class

--- DoubleMatrix33.cpp
+++ DoubleMatrix33.cpp
@@ -1,1 +1,19 @@
 #include "DoubleMatrix33.h"
+
+// Constructors
+DoubleMatrix33::DoubleMatrix33()
+	: column0(), column1(), column2() {}
+DoubleMatrix33::DoubleMatrix33(const DoubleVec3D& column0, const DoubleVec3D& column1, const DoubleVec3D& column2)
+	: column0(column0), column1(column1), column2(column2) {}
+DoubleMatrix33::DoubleMatrix33(const DoubleMatrix33& matrix)
+	: column0(matrix.column0), column1(matrix.column1), column2(matrix.column2) {}
+
+// Setters
+DoubleVec3D DoubleMatrix33::getColumn0() { return column0; }
+DoubleVec3D DoubleMatrix33::getColumn1() { return column1; }
+DoubleVec3D DoubleMatrix33::getColumn2() { return column2; }
+
+// Getters
+void DoubleMatrix33::setColumn0(const DoubleVec3D& column0) { this->column0 = column0; }
+void DoubleMatrix33::setColumn1(const DoubleVec3D& column1) { this->column1 = column1; }
+void DoubleMatrix33::setColumn2(const DoubleVec3D& column2) { this->column2 = column2; }
--- DoubleMatrix33.h
+++ DoubleMatrix33.h
@@ -1,8 +1,26 @@
 #ifndef DEF_DOUBLEMATRIX33
 #define DEF_DOUBLEMATRIX33
 
-class DoubleMatrix33 {
+#include "DoubleVec3D.h"
 
+class DoubleMatrix33 {
+private:
+	DoubleVec3D column0;
+	DoubleVec3D column1;
+	DoubleVec3D column2;
+
+public:
+	DoubleMatrix33();
+	DoubleMatrix33(const DoubleVec3D& column0, const DoubleVec3D& column1, const DoubleVec3D& column2);
+	DoubleMatrix33(const DoubleMatrix33& matrix);
+
+	DoubleVec3D getColumn0();
+	DoubleVec3D getColumn1();
+	DoubleVec3D getColumn2();
+
+	void setColumn0(const DoubleVec3D& column0);
+	void setColumn1(const DoubleVec3D& column1);
+	void setColumn2(const DoubleVec3D& column2);
 };
 
 
