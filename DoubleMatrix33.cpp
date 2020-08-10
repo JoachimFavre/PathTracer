2f3d59aef408a0cf8ccdab6dc87032f14f76c338
Joachim Favre <joachim@favre.xyz>
2020-08-10 12:56:24 +0200

    Defined getRow...T() and operators for matrices

--- DoubleMatrix33.cpp
+++ DoubleMatrix33.cpp
@@ -1,19 +1,91 @@
 #include "DoubleMatrix33.h"
 
 // Constructors
-DoubleMatrix33::DoubleMatrix33()
-	: column0(), column1(), column2() {}
+DoubleMatrix33::DoubleMatrix33(double val = 0)
+	: column0(val), column1(val), column2(val) {}
 DoubleMatrix33::DoubleMatrix33(const DoubleVec3D& column0, const DoubleVec3D& column1, const DoubleVec3D& column2)
 	: column0(column0), column1(column1), column2(column2) {}
 DoubleMatrix33::DoubleMatrix33(const DoubleMatrix33& matrix)
 	: column0(matrix.column0), column1(matrix.column1), column2(matrix.column2) {}
 
-// Setters
-DoubleVec3D DoubleMatrix33::getColumn0() { return column0; }
-DoubleVec3D DoubleMatrix33::getColumn1() { return column1; }
-DoubleVec3D DoubleMatrix33::getColumn2() { return column2; }
+
+// Getters
+DoubleVec3D DoubleMatrix33::getColumn0() const { return column0; }
+DoubleVec3D DoubleMatrix33::getColumn1() const { return column1; }
+DoubleVec3D DoubleMatrix33::getColumn2() const { return column2; }
+
+DoubleVec3D DoubleMatrix33::getRow0T() const {
+	return DoubleVec3D(column0.getX(), column1.getX(), column2.getX()); 
+}
+
+DoubleVec3D DoubleMatrix33::getRow1T() const {
+	return DoubleVec3D(column0.getY(), column1.getY(), column2.getY());
+}
+
+DoubleVec3D DoubleMatrix33::getRow2T() const {
+	return DoubleVec3D(column0.getZ(), column1.getZ(), column2.getZ());
+}
+
 
 // Getters
 void DoubleMatrix33::setColumn0(const DoubleVec3D& column0) { this->column0 = column0; }
 void DoubleMatrix33::setColumn1(const DoubleVec3D& column1) { this->column1 = column1; }
 void DoubleMatrix33::setColumn2(const DoubleVec3D& column2) { this->column2 = column2; }
+
+
+// Operators
+void DoubleMatrix33::operator+=(const DoubleMatrix33& matrix) {
+	column0 += matrix.column0;
+	column1 += matrix.column1;
+	column2 += matrix.column2;
+}
+
+void DoubleMatrix33::operator-=(const DoubleMatrix33& matrix) {
+	operator+=(-matrix);
+}
+
+void DoubleMatrix33::operator*=(const DoubleMatrix33& matrix) {
+	DoubleMatrix33 thisCopy = *this;
+
+	column0 = thisCopy*matrix.getColumn0();
+	column1 = thisCopy*matrix.getColumn1();
+	column2 = thisCopy*matrix.getColumn2();
+}
+
+void DoubleMatrix33::operator*=(double val) {
+	column0 *= val;
+	column1 *= val;
+	column2 *= val;
+}
+
+
+// "Extern" operators
+DoubleMatrix33 operator+(const DoubleMatrix33& matrix1, const DoubleMatrix33& matrix2) {
+	DoubleMatrix33 result = matrix1;
+	result += matrix2;
+	return result;
+}
+
+DoubleMatrix33 operator-(const DoubleMatrix33& matrix) {
+	DoubleMatrix33 copy = matrix;
+	copy *= -1;
+	return copy;
+}
+
+DoubleMatrix33 operator-(const DoubleMatrix33& matrix1, const DoubleMatrix33& matrix2) {
+	DoubleMatrix33 result = matrix1;
+	result -= matrix2;
+	return result;
+}
+
+DoubleMatrix33 operator*(const DoubleMatrix33& matrix1, const DoubleMatrix33& matrix2) {
+	DoubleMatrix33 result = matrix1;
+	result *= matrix2;
+	return result;
+}
+
+DoubleVec3D operator*(const DoubleMatrix33& matrix, const DoubleVec3D& vec) {
+	return DoubleVec3D(dotProd(matrix.getRow0T(), vec),
+		               dotProd(matrix.getRow1T(), vec),
+		               dotProd(matrix.getRow2T(), vec));
+}
--- DoubleMatrix33.h
+++ DoubleMatrix33.h
@@ -10,18 +10,34 @@ private:
 	DoubleVec3D column2;
 
 public:
-	DoubleMatrix33();
+	DoubleMatrix33(double val=0);
 	DoubleMatrix33(const DoubleVec3D& column0, const DoubleVec3D& column1, const DoubleVec3D& column2);
 	DoubleMatrix33(const DoubleMatrix33& matrix);
 
-	DoubleVec3D getColumn0();
-	DoubleVec3D getColumn1();
-	DoubleVec3D getColumn2();
+	DoubleVec3D getColumn0() const;
+	DoubleVec3D getColumn1() const;
+	DoubleVec3D getColumn2() const;
+
+	DoubleVec3D getRow0T() const;
+	DoubleVec3D getRow1T() const;
+	DoubleVec3D getRow2T() const;
 
 	void setColumn0(const DoubleVec3D& column0);
 	void setColumn1(const DoubleVec3D& column1);
 	void setColumn2(const DoubleVec3D& column2);
+
+	void operator+=(const DoubleMatrix33& matrix);
+	void operator-=(const DoubleMatrix33& matrix);
+	void operator*=(const DoubleMatrix33& matrix);
+	void operator*=(double val);
 };
+
+DoubleMatrix33 operator+(const DoubleMatrix33& matrix1, const DoubleMatrix33& matrix2);
+DoubleMatrix33 operator-(const DoubleMatrix33& matrixs);
+DoubleMatrix33 operator-(const DoubleMatrix33& matrix1, const DoubleMatrix33& matrix2);
+DoubleMatrix33 operator*(const DoubleMatrix33& matrix1, const DoubleMatrix33& matrix2);
+DoubleVec3D operator*(const DoubleMatrix33& matrix, const DoubleVec3D& vec);
+
 
 
 #endif
