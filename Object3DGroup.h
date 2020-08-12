f36b28f9e96a1648d6cdb4b2a5f6ad8696833df1
Joachim Favre <joachim@favre.xyz>
2020-08-12 10:17:45 +0200

    Defined the header for the Object3DGroup class

--- Object3DGroup.h
+++ Object3DGroup.h
@@ -1,8 +1,34 @@
 #ifndef DEF_OBJECT3DGROUP
 #define DEF_OBJECT3DGROUP
 
-class Object3DGroup {
+#include <string>
+#include <vector>
 
+#include "DoubleVec3D.h"
+#include "Object3D.h"
+
+class Object3DGroup {
+private:
+	std::string name;
+	std::vector<Object3D*> objects;
+	DoubleVec3D center;
+
+public:
+	Object3DGroup();
+	Object3DGroup(std::string name, std::initializer_list<Object3D*> objects);
+	Object3DGroup(const Object3DGroup& group);
+
+	std::string getName() const;
+	std::vector<Object3D*> getObjects() const;
+	DoubleVec3D getCenter() const;
+
+	void setName(const std::string& name);
+	void addObject(Object3D* object);
+	void addObjects(const std::vector<Object3D*>& objects);
+	void merge(const Object3DGroup& group);
+	void resetObjects();
 };
+
+std::ostream& operator<<(std::ostream& stream, const Object3DGroup& group);
 
 #endif
