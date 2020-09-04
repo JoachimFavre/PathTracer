86926e09268e711e077abdafcbe4764967cc8824
Joachim Favre <joachim@favre.xyz>
2020-09-04 18:58:56 +0200

    Removed unused code from main

--- main.cpp
+++ main.cpp
@@ -1,58 +1,8 @@
-#include <string>
-#include <iomanip>
-#include <iostream>
-
-#define _USE_MATH_DEFINES  // to be able to use M_PI from math.h
-#include <math.h>
-#include <omp.h>
-
-#include "nlohmann/json.hpp"
-using json = nlohmann::json;
-
-#include "Material.h"
-#include "DiffuseMaterial.h"
-#include "DoubleMatrix33.h"
-#include "DoubleVec3D.h"
-#include "DoubleUnitVec3D.h"
-#include "Object3D.h"
-#include "PerspectiveCamera.h"
-#include "RefractiveMaterial.h"
-#include "SpecularMaterial.h"
-#include "Sphere.h"
-#include "Triangle.h"
 #include "Interface.h"
-
-#include "Scene.h"
 
 int main() {
 	initInterface();
-	
 
-	/*
-	double beginningTime = getCurrentTimeSeconds();
-	// Display doubles with 2 decimals
-
-	// Instantiate camera and scene
-	PerspectiveCamera camera(PICTURE_WIDTH, PICTURE_HEIGHT, CAMERA_FOCAL_LENGTH, CAMERA_FOV_X);
-	Scene scene(camera, SAMPLE_PER_PIXEL, MIN_BOUNCES, MAX_DEPTH, RR_STOP_PROBABILITY);
-	scene.setRussianRoulette(RUSSIAN_ROULETTE);
-	scene.setNextEventEstimation(NEXT_EVENT_ESTIMATION);
-	scene.setNumberThreads(NUMBER_THREADS);
-
-	if (useDefaultScene) {
-		scene.defaultScene();
-	} else { // Read file
-		// scene.importFBX(FILE_PATH);
-		// scene.addObject(new Sphere(DoubleVec3D(0, 1.5, -2.5), 0.5, new DiffuseMaterial(DoubleVec3D(0), 4000)));
-	}
-
-	// Render and write picture
-	scene.render()->writeToFile(MIDDLE_GRAY);
-
-	// Measure time and make sound to warn that it is ended
-	std::cout << std::endl << "Total computation time: " << getCurrentTimeSeconds() - beginningTime << " seconds." << std::endl;
-	std::cout << "\a";
-	*/
-	
 	return 0;
 }
+
