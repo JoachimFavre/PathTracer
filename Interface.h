4e1e3271c4fa9fb17d263b2c860dd330b00cf113
Joachim Favre <joachim@favre.xyz>
2020-09-04 18:49:13 +0200

    Moved functions from main to interface

--- Interface.h
+++ Interface.h
@@ -1,7 +1,29 @@
 #ifndef INTERFACE
 #define INTERFACE
 
-#include "InterfaceGestion.h"
+#include <iomanip>
 
+#include "InterfaceGestion.h"
+#include "Scene.h"
+#include "PerspectiveCamera.h"
+#include "Object3DGroup.h"
+
+enum class Page {
+	ParametersPage,
+	ObjectsPage
+};
+
+static Scene scene;
+static PerspectiveCamera& camera = scene.getCameraReference();
+static std::vector<Object3DGroup>& objectGroups = scene.getObjectsGroupsReference();
+static Page currentPage = Page::ParametersPage;
+static bool commandWasInvalid = false;
+
+void clearScreenPrintHeader();
+void receiveAndExecuteGeneralCommands();
+void executeParametersCommands(char command);
+void executeObjectsCommands(char command);
+void displayCommands();
+void initInterface();
 
 #endif
