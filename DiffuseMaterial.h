68c36ac73e3d078b31a9d64dc7709d9f7d6ea98d
Joachim Favre <joachim@favre.xyz>
2020-10-19 22:23:46 +0200

    Moved documentation at the beginning of DiffuseMaterial.h

--- DiffuseMaterial.h
+++ DiffuseMaterial.h
@@ -10,89 +10,90 @@
 #include "Ray.h"
 
 
-//! Models a diffuse material
+/*!
+	\file DiffuseMaterial.h
+	\brief Defines the DiffuseMaterial class.
+
+	\class DiffuseMaterial
+	\brief Models a diffuse material
+
+	\fn DiffuseMaterial::DiffuseMaterial(const DoubleVec3D& colour = DoubleVec3D(0.0), DoubleVec3D emittance = 0)
+	\brief Default and main constructor.
+	\param colour The colour of this material.
+	\param emittance The light this material emits.
+
+	\fn DiffuseMaterial::DiffuseMaterial(const DiffuseMaterial& material)
+	\brief Copy constructor.
+	\param material The material that will be copied.
+
+	\fn DiffuseMaterial::getColour()
+	\brief Getter for the colour attribute.
+	\return The colour of this material.
+	\sa setColour()
+
+	\fn DiffuseMaterial::setColour(const DoubleVec3D& colour)
+	\brief Getter for the colour attribute.
+	\param colour The new colour of this material.
+	\sa getColour()
+
+	\fn DiffuseMaterial::deepCopy()
+	\brief Makes a deep copy of this material.
+	\return A pointer to a deeply copied version of this material.
+
+	\fn DiffuseMaterial::getNewDirection(const Ray& previousRay, const DoubleUnitVec3D& normal, double (*randomDouble)())
+	\brief Computes the new ray direction.
+	\param previousRay The ray that hits this material.
+	\param normal The normal at the intersection.
+	\param randomDouble A pointer to a function generating a random double between 0 and 1.
+
+	\fn DiffuseMaterial::computeCurrentColour(const DoubleVec3D& recursiveColour, double angleNewDirectionNormal)
+	\brief Computes the new colour.
+	\param recursiveColour The colour recursively obtained.
+	\param angleNewDirectionNormal The angle between the cosine of the angle between the next ray's direction and the normal at the intersection.
+	\return The new colour.
+
+	\fn DiffuseMaterial::worksWithNextEventEstimation()
+	\brief Returns whether this material works with next event estimation.
+	\return True.
+
+	\fn DiffuseMaterial::getDescription(std::ostream& stream)
+	\brief Returns this material's description.
+	\param stream The current stream.
+	\return The stream with the description.
+
+	\fn DiffuseMaterial::getType()
+	\brief Returns "Diffuse".
+
+	\fn DiffuseMaterial::getSpecificParametersJson()
+	\brief Converts this material's specific parameters to json.
+	\return This material's specific parameters converted to json.
+
+	\fn DiffuseMaterial::setSpecificParametersJson(const json& j)
+	\brief Sets this material's specific parameters according to json.
+	\param j The json input.
+*/
+
+
 class DiffuseMaterial : public Material {
 private:
 	DoubleVec3D colour;
 
 public:
-	//! Default and main constructor.
-	/*!
-		\param colour The colour of this material.
-		\param emittance The light this material emits.
-	*/
 	DiffuseMaterial(const DoubleVec3D& colour = DoubleVec3D(0.0), DoubleVec3D emittance = 0);
-
-	//! Copy constructor.
-	/*!
-		\param material The material that will be copied.
-	*/
 	DiffuseMaterial(const DiffuseMaterial& material);
 
-
-	//! Getter for the colour attribute.
-	/*!
-		\return The colour of this material.
-		\sa setColour()
-	*/
 	DoubleVec3D getColour() const;
-	
-	//! Getter for the colour attribute.
-	/*!
-		\param colour The new colour of this material.
-		\sa getColour()
-	*/
 	void setColour(const DoubleVec3D& colour);
 
-
-	//! Makes a deep copy of this material.
-	/*!
-		\return A pointer to a deeply copied version of this material.
-	*/
 	Material* deepCopy() const;
 
-	//! Computes the new ray direction.
-	/*!
-		\param previousRay The ray that hits this material.
-		\param normal The normal at the intersection.
-		\param randomDouble A pointer to a function generating a random double between 0 and 1.
-	*/
 	DoubleUnitVec3D getNewDirection(const Ray& previousRay, const DoubleUnitVec3D& normal, double (*randomDouble)()) const;
-
-	//! Computes the new colour.
-	/*!
-		\param recursiveColour The colour recursively obtained.
-		\param angleNewDirectionNormal The angle between the cosine of the angle between the next ray's direction and the normal at the intersection.
-		\return The new colour.
-	*/
 	DoubleVec3D computeCurrentColour(const DoubleVec3D& recursiveColour, double angleNewDirectionNormal) const;
-	
-	//! Returns whether this material works with next event estimation.
-	/*!
-		\return True.
-	*/
 	bool worksWithNextEventEstimation() const;
 
-	//! Returns this material's description.
-	/*!
-		\param stream The current stream.
-		\return The stream with the description.
-	*/
 	std::ostream& getDescription(std::ostream& stream) const;
-
-	//! Returns "Diffuse".
 	std::string getType() const;
-
-	//! Converts this material's specific parameters to json.
-	/*!
-		\return This material's specific parameters converted to json.
-	*/
 	json getSpecificParametersJson() const;
-
-	//! Sets this material's specific parameters according to json.
-	/*!
-		\param j The json input.
-	*/
 	void setSpecificParametersJson(const json& j);
 };
 
