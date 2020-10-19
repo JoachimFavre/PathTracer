f433b923333848364300138c3350e61264e126c4
Joachim Favre <joachim@favre.xyz>
2020-10-19 22:03:43 +0200

    Moved documentation at the beginning of the file

--- Interface.h
+++ Interface.h
@@ -8,18 +8,45 @@
 #include "PerspectiveCamera.h"
 #include "Object3DGroup.h"
 
-//! Different current main pages possible.
 /*!
-	Could have been defined using a boolean, but it is not something that really needs to be optimised and an enumeration is, I think, more readable.
+	\file Interface.h
+	\brief Defines some functions for the interface.
+
+	\enum Page
+	\brief Different current main pages possible.
+	\details Could have been defined using a boolean, but it is not something that really needs to be optimised and an enumeration is, I think, more readable.
+
+	\var smallestRenderTime4PictBackup
+	\brief If the render time takes more time, the render will be backed up.
+
+	\var backupFileName
+	\brief The name of the file in which the backup will be made.
+
+	\fn receiveAndExecuteGeneralCommands()
+	\brief Receives and executes the general commands.
+	\details Asks a command to the user. If it is general to both main pages (exit, for example), executes it, else, calls executeParametersCommands() or exercuteObjectsCommands().
+
+	\fn executeParametersCommands(char command)
+	\brief Executes a command specific to the parameters page.
+
+	\fn executeObjectsCommands(char command)
+	\brief Executes a command specific to the objects page.
+
+	\fn displayCommands()
+	\brief Prints the available command.
+	\details The available commands change depending on the active page.
+
+	\fn initInterface()
+	\brief Starts the infinite loop of the interface.
 */
+
 enum class Page {	
 	ParametersPage,
 	ObjectsPage
 };
 
-//! If the render time takes more time, the render will be backed up. 
+
 constexpr double smallestRenderTime4PictBackup = 180.0;  // Three minutes
-//! The name of the file in which the backup will be made.
 const std::string backupFileName = formatFileName("backup", PICTURE_SAVE_EXTENSION_JSON);
 
 static Scene scene;
@@ -29,26 +56,11 @@ static Page currentPage = Page::ParametersPage;
 static bool commandWasInvalid = false;
 
 // Main interface
-
-//! Receives and executes the general commands.
-/*!
-	Asks a command to the user. If it is general to both main pages (exit, for example), executes it, else, calls executeParametersCommands() or exercuteObjectsCommands().
-*/
 void receiveAndExecuteGeneralCommands();
-
-//! Executes a command specific to the parameters page.
 void executeParametersCommands(char command);
-
-//! Executes a command specific to the objects page.
 void executeObjectsCommands(char command);
 
-//! Prints the available command.
-/*!
-	The available commands change depending which page is active.
-*/
 void displayCommands();
-
-//! Starts the infinite loop of the interface.
 void initInterface();
 
 #endif
