5a4b2ac0dde486ebb34f4dc056bed945c279325d
Joachim Favre <joachim@favre.xyz>
2020-10-20 10:36:13 +0200

    Moved documentation at the beginning of Ray.h

--- Ray.h
+++ Ray.h
@@ -4,56 +4,57 @@
 #include "DoubleVec3D.h"
 #include "DoubleUnitVec3D.h"
 
-//! Combination of an origin and a direction.
+/*!
+	\file Ray.h
+	\brief Defines the Ray class.
+
+	\fn class Ray
+	\brief Combination of an origin and a direction.
+
+	\fn Ray::Ray()
+	\brief Default constructor.
+	\details By default, the origin is at (0, 0, 0) and the direction is (1, 0, 0)
+
+	\fn Ray::Ray(const DoubleVec3D& origin, const DoubleUnitVec3D& direction)
+	\brief Main constructor.
+	\param origin Where the ray starts.
+	\param direction The ray direction.
+
+	\fn Ray::Ray(const Ray& ray)
+	\brief Copy constructor.
+	\param ray The ray that will be copied.
+
+	\fn Ray::getOrigin()
+	\brief Getter for the origin.
+	\return This ray's origin.
+
+	\fn Ray::getDirection()
+	\brief Getter for the direction.
+	\return This ray's direction.
+
+	\fn Ray::setOrigin(const DoubleVec3D& origin)
+	\brief Setter for the origin.
+	\param origin The new origin of this ray.
+
+	\fn Ray::setDirection(const DoubleUnitVec3D& direction)
+	\brief Setter for the direction.
+	\param direction The new direction of this ray.
+*/
+
 class Ray {
 private:
 	DoubleVec3D origin;
 	DoubleUnitVec3D direction;
 
 public:
-	//! Default constructor.
-	/*!
-		By default, the origin is at (0, 0, 0) and the direction is (1, 0, 0)
-	*/
 	Ray();
-
-	//! Main constructor.
-	/*!
-		\param origin Where the ray starts.
-		\param direction The ray direction.
-	*/
 	Ray(const DoubleVec3D& origin, const DoubleUnitVec3D& direction);
-
-	//! Copy constructor.
-	/*!
-		\param ray The ray that will be copied.
-	*/
 	Ray(const Ray& ray);
 
-
-	//! Getter for the origin.
-	/*!
-		\return This ray's origin.
-	*/
 	DoubleVec3D getOrigin() const;
-
-	//! Getter for the direction.
-	/*!
-		\return This ray's direction.
-	*/
 	DoubleUnitVec3D getDirection() const;
 
-
-	//! Setter for the origin.
-	/*!
-		\param origin The new origin of this ray.
-	*/
 	void setOrigin(const DoubleVec3D& origin);
-
-	//! Setter for the direction.
-	/*!
-		\param direction The new direction of this ray.
-	*/
 	void setDirection(const DoubleUnitVec3D& direction);
 };
 
